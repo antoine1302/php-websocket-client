d71332c83f2819cec8ec9cfe93974d0dd3169140
RICHARD Georges-Antoine <garichard85@gmail.com>
2023-01-23 02:28:23 +0100

    implements methof read/write for streamsocketHandler

--- src/Service/Handshake/KeyValidator.php
+++ src/Service/Handshake/KeyValidator.php
@@ -7,8 +7,10 @@ namespace Totoro1302\PhpWebsocketClient\Service\Hansdshake;
 class KeyValidator
 {
     private const HANDSHAKE_MAGIC_STRING = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
-    public function validate(): bool
+    public function validate(string $websocketKey, string $websocketKeyAccept): bool
     {
-        return true;
+        $keyExpected = base64_encode(sha1($websocketKey . self::HANDSHAKE_MAGIC_STRING, true));
+
+        return $websocketKeyAccept === $keyExpected;
     }
 }
