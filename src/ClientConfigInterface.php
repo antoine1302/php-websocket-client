ecdcc8da872d3356a5a49a48ff3adbf943f1fe51
Antoine1302 <garichard85@gmail.com>
2023-01-25 23:25:29 +0100

    Refacto Client & StreamSocketHandler (#9)

--- src/ClientConfigInterface.php
+++ src/ClientConfigInterface.php
@@ -9,6 +9,7 @@ interface ClientConfigInterface
     public function getName(): string;
     public function getUri(): string;
     public function getConnectionTimeout(): ?int;
+    public function getOrigin(): ?string;
     public function isPersistent(): ?bool;
     public function getSubProtocols(): ?array;
     
--- src/Exception/ClientException.php
+++ src/Exception/ClientException.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 namespace Totoro1302\PhpWebsocketClient\Exception;
 
-class StreamSocketConnectionException extends \RuntimeException
+class ClientException extends \RuntimeException
 {
 }
